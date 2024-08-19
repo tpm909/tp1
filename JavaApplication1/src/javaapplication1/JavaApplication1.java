2bc075c71ab7735914165c5156593ecb6787f453
tpm909 <ankitoperez909@gmail.com>
2024-08-19 17:30:24 -0300

    fdsac
    sdf

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -14,9 +14,10 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("fsdadwsfafthfhdaos");
+        System.out.println("fsdadwsfos");
         System.out.println("Hola");
         System.out.println("Segundo commit");
+        System.out.println("cosas");
     }
     
 }
