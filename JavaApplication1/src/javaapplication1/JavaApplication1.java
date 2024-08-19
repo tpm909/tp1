087a193e9268809d5d91c8bfa6ea033a426920c1
mfzini <miguelfzini@protonmail.ch>
2024-08-19 17:16:28 -0300

    Saludo

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -15,6 +15,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("fsdadwsfafthfhdaos");
+        System.out.println("Hola");
     }
     
 }
